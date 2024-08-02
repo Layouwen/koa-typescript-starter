ae9234221901ebaed493ac05d338b66d60c7050b
Avan <layouwen@gmail.com>
2024-08-02 12:13:24 +0800

    chore: add eslint and prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,40 @@
-import antfu from '@antfu/eslint-config'
+import antfu from '@antfu/eslint-config';
 
-export default antfu({})
+export default antfu(
+  {
+    formatters: {
+      css: true,
+      html: true,
+      markdown: true,
+    },
+    stylistic: {
+      indent: 2,
+    },
+    ignores: ['tsconfig.app.json'],
+  },
+  {
+    rules: {
+      'style/semi': ['error', 'always'],
+      'style/member-delimiter-style': [
+        'error',
+        {
+          multiline: {
+            delimiter: 'semi',
+            requireLast: true,
+          },
+          singleline: {
+            delimiter: 'semi',
+            requireLast: false,
+          },
+          multilineDetection: 'brackets',
+        },
+      ],
+      'no-console': [
+        'error',
+        {
+          allow: ['warn', 'error'],
+        },
+      ],
+    },
+  },
+);

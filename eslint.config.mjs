d294b696a53c82519cca8ea477563739811b7d17
Avan <layouwen@gmail.com>
2025-01-07 11:23:59 +0800

    feat: use @avanlan/eslint-config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,40 +1,7 @@
-import antfu from '@antfu/eslint-config';
+import avanlan from '@avanlan/eslint-config';
 
-export default antfu(
+export default avanlan(
   {
-    formatters: {
-      css: true,
-      html: true,
-      markdown: true,
-    },
-    stylistic: {
-      indent: 2,
-    },
     ignores: ['tsconfig.app.json'],
-  },
-  {
-    rules: {
-      'style/semi': ['error', 'always'],
-      'style/member-delimiter-style': [
-        'error',
-        {
-          multiline: {
-            delimiter: 'semi',
-            requireLast: true,
-          },
-          singleline: {
-            delimiter: 'semi',
-            requireLast: false,
-          },
-          multilineDetection: 'brackets',
-        },
-      ],
-      'no-console': [
-        'error',
-        {
-          allow: ['warn', 'error'],
-        },
-      ],
-    },
   },
 );
